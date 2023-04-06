bed8008b28268fa438a136acc4a019ad553a72af
Lê Quang Minh <106050463+minhlqBHsoft@users.noreply.github.com>
2023-04-06 17:13:34 -0400

    Update reservation form

--- dbliv2/home_customer.php
+++ dbliv2/home_customer.php
@@ -4,13 +4,28 @@ include("connection.php");
 include("function.php");
 $client_data = check_login_customer($con);
 
-$_SESSION;
+// $_SESSION;
 
 if (isset($_POST['reserve'])) {
-	$client_SSN = $_POST['client_id'];
+	$client_SSN = $client_data['client_SSN'];
+	echo "$client_SSN";
 	$room_id = $_POST['room_id'];
+	echo "$room_id";
 	$startDate = $_POST['startDate'];
+	echo "$startDate";
 	$endDate = $_POST['endDate'];
+	echo "$endDate";
+
+	$query ="INSERT INTO Reservation (client_SSN, room_id, startDate, endDate, archived) values ('$client_SSN', $room_id, 'jghasdf', 'gnfcgfd', 0)";
+	try{
+		$result = mysqli_query($con, $query);
+ 	}catch (Exception $e){
+		 echo "<p style= \"color:red\"> Reservation counldn't be created! Try again!</p>";
+		 echo $e->getMessage();
+		 //exit;
+ 	}
+
+	
 
 }
 
@@ -30,15 +45,18 @@ if (isset($_POST['reserve'])) {
 	<script src="scripts/web/ej.web.all.min.js"> </script>
 
 	<!-- Date picker -->
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/css/bootstrap-datepicker.min.css">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+		integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+	<link rel="stylesheet"
+		href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/css/bootstrap-datepicker.min.css">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/js/bootstrap-datepicker.min.js"></script>
+	<script
+		src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/js/bootstrap-datepicker.min.js"></script>
 
 </head>
-<style type="text/css"> 
+<style type="text/css">
 	.form {
 		padding: 30px;
 		outline: solid 1px #026afa;
@@ -46,12 +64,13 @@ if (isset($_POST['reserve'])) {
 		border-radius: 10px;
 		margin-bottom: 50px;
 	}
+
 	/* #reservation_date {
 		display: none;
 	} */
 
-	.hidden{
- 		display: none;
+	.hidden {
+		display: none;
 	}
 </style>
 
@@ -73,53 +92,7 @@ if (isset($_POST['reserve'])) {
 			<div class="col-sm-8">
 				<?php echo $deleteMsg ?? ''; ?>
 				<div class="table-responsive">
-					<!-- <table class="table table-bordered" >
-			<thead><tr><th>room_id</th>
-
-				<th>room_number</th>
-				<th>hotel_id</th>
-				<th>price</th>
-				<th>peopleCapacity</th>
-				<th>view</th>
-				<th>extandable </th>
-				<th>damage</th>
-				<th>chain_name</th>
-				<th>star rating (1-5)</th>
-				<th>city</th>
-			</thead>
-			<tbody>
-		<?php
-		if (is_array($fetchData)) {
-
-			foreach ($fetchData as $data) {
-				?>
-		</div>
-			<tr>
-			<td><?php echo $data['room_id'] ?? ''; ?></td>
-			<td><?php echo $data['room_number'] ?? ''; ?></td>
-			<td><?php echo $data['hotel_id'] ?? ''; ?></td>
-			<td><?php echo $data['price'] ?? ''; ?></td>
-			<td><?php echo $data['peopleCapacity'] ?? ''; ?></td>
-			<td><?php echo $data['view'] ?? ''; ?></td>
-			<td><?php echo $data['extandable '] ?? ''; ?></td>
-			<td><?php echo $data['damage'] ?? ''; ?></td>
-			<td><?php echo $data['chain_name'] ?? ''; ?></td>
-			<td><?php echo $data['ratingStars'] ?? ''; ?></td>
-			<td><?php echo $data['city'] ?? ''; ?></td>  
-			<td> <input type="button" value="<?php echo $data['room_id'] ?? ''; ?>" onclick= /></td>
-			</tr>
-			<?php
-			}
-		} else { ?>
-			<tr>
-				<td colspan="8">
-			<?php echo $fetchData; ?>
-		</td>
-			<tr>
-			<?php
-		} ?>
-			</tbody>
-			</table> -->
+					
 				</div>
 			</div>
 		</div>
@@ -130,48 +103,77 @@ if (isset($_POST['reserve'])) {
 				foreach ($fetchData as $data) {
 					?>
 					<div class="form">
-						<form method="post">
+						<form>
 							<h2>
 								<?php echo $data['chain_name'] ?? ''; ?>
 							</h2>
 							<div class="mb-3">
-								<h4><?php echo $data['ratingStars'] ?? ''; ?> &#11088; </h4>
+								<h4>
+									<?php echo $data['ratingStars'] ?? ''; ?> &#11088;
+								</h4>
 							</div>
 							<div class="mb-3">
-								<h4>Location: <?php echo $data['city'] ?? ''; ?></h4>
+								<h4>Location:
+									<?php echo $data['city'] ?? ''; ?>
+								</h4>
 							</div>
 							<div class="mb-3">
-								<h4>Room number: <?php echo $data['room_number'] ?? ''; ?></h4>
+								<h4>Room number:
+									<?php echo $data['room_number'] ?? ''; ?>
+								</h4>
+							</div>
+							<div class="mb-3 hidden">
+								<h4>Room id:
+									<h4 name = "room_id"><?php echo $data['room_id'] ?? ''; ?></h4>
+								</h4>
 							</div>
 							<div class="mb-3">
-								<h4>View: <?php echo $data['view'] ?? ''; ?></h4>
+								<h4>View:
+									<?php echo $data['view'] ?? ''; ?>
+								</h4>
 							</div>
 							<div class="mb-3">
-								<h4>Price (per night): <?php echo $data['price'] ?? ''; ?></h4>
+								<h4>Price (per night):
+									<?php echo $data['price'] ?? ''; ?>
+								</h4>
 							</div>
-							
 
-							<button type="button" class="btn btn-primary" id="hide-show">Reserve</button>
+							<!-- Button trigger modal -->
+							<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+								Reserve
+							</button>
 
-							<div id="reservation_date">
-								<div class="mb-3">
-									<label for="startDate">Start Date</label>
-									<input id="startDate" class="form-control" type="date" name="startDate"/>
+							<!-- Modal -->
+							<div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false"
+								tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+								<div class="modal-dialog modal-dialog-centered">
+									<div class="modal-content">
+										<div class="modal-header">
+											<h1 class="modal-title fs-5" id="staticBackdropLabel">Modal title</h1>
+											<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+										</div>
+										<div class="modal-body">
+											<div id="reservation_date">
+												<div class="mb-3">
+													<label for="startDate">Start Date</label>
+													<input id="startDate" class="form-control" type="date" name="startDate" required/>
+												</div>
+												<div class="mb-3">
+													<label for="startDate">End Date</label>
+													<input id="endDate" class="form-control" type="date" name="endDate" required/>
+												</div>
+												<div class="mb-3">
+													<label for="startDate">Total Price:</label>
+												</div>
+
+											</div>
+										</div>
+										<div class="modal-footer">
+											<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+											<button type="submit" class="btn btn-primary" name="reserve">Confirm Reserve</button>
+										</div>
+									</div>
 								</div>
-								<div class="mb-3" >
-									<label for="startDate">End Date</label>
-									<input id="endDate" class="form-control" type="date" name="endDate"/>
-								</div>
-								<div class="mb-3" >
-									<label for="startDate">Total Price:</label>
-								</div>
-
-								<script>
-									var startDate = document.getElementById("endDate").value;
-									console.log(startDate);
-								</script>
-
-								<button type="submit" class="btn btn-primary" name="reserve">Confirm Reserve</button>
 							</div>
 
 						</form>
@@ -192,24 +194,16 @@ if (isset($_POST['reserve'])) {
 </body>
 
 <!-- Script for reserve trigger -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
+	integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 <script>
-setHiddable("hide-show", "reservation_date");
 
-/**
- * Ideal to set a hide/show relationship between only two elements
- * @param {string} btnId The ID of the button that toggle the view
- * @param {string} elementId The ID of the element to be toggled
- */
-function setHiddable(btnId, elementId) {
-    const btn = document.querySelector(`#${btnId}`);
+	const myModal = document.getElementById('myModal')
+	const myInput = document.getElementById('myInput')
 
-    // Listen to clicks
-    btn.addEventListener("click", (ev) => {
-        const targetEl = document.querySelector(`#${elementId}`);
-        // Hide if shown
-        // Show if hidden
-        targetEl.classList.toggle("hidden");
-    });
-}
+	myModal.addEventListener('shown.bs.modal', () => {
+		myInput.focus()
+	})
 </script>
+
 </html>
